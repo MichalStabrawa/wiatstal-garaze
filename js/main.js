c8f1f8531fbe802974ee565f037bed595df1013f
Michal Stabrawa <mistaaqua@gmail.com>
2018-05-12 13:51:11 +0200

    calc

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
+
+
+
 $(document).ready(function(){
     $('.hamburger-menu').click(function(){
         $('.menu-navbar').toggle();
     })
     
-    $('#garageSpadTyl').click(function(){
-           $('#figure-spad-tyl').show();
-    })
+$("#test").click(function(){
+    $("#figure-spad-tyl").show();
+})
      
   
 })
