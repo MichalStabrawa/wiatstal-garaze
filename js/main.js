06e24252c97140fe7bc17d582a865b300d791683
Michal Stabrawa <mistaaqua@gmail.com>
2018-05-12 13:31:58 +0200

    calc garaze

--- js/main.js
+++ js/main.js
@@ -2,7 +2,17 @@ $(document).ready(function(){
     $('.hamburger-menu').click(function(){
         $('.menu-navbar').toggle();
     })
+    
+    $('#garageSpadTyl').click(function(){
+           $('#figure-spad-tyl').show();
+    })
+     
+  
 })
+
+/*calculator garage function*/
+
+
 
 
 
