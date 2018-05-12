e59f87dd97e0bd8d45d0189927021f5c2e6c27c9
Michal Stabrawa <mistaaqua@gmail.com>
2018-05-12 16:24:50 +0200

    calculator first function

--- js/main.js
+++ js/main.js
@@ -1,22 +1,39 @@
-
-
-
-$(document).ready(function(){
-    $('.hamburger-menu').click(function(){
+$(document).ready(function () {
+    $('.hamburger-menu').click(function () {
         $('.menu-navbar').toggle();
     })
-    
-$("#test").click(function(){
-    $("#figure-spad-tyl").show();
-})
-     
-  
+
+    $("#test").click(function () {
+        $("#figure-spad-tyl").show();
+    })
+
+
 })
 
 /*calculator garage function*/
+var cena1 = document.getElementById("test2");
+
+function sprawdz() {
+
+
+    var select1 = document.getElementById('szerokosc').value;
+    var select2 = document.getElementById('dlugosc').value;
+
+    var roznicaDlugosc = (parseFloat(select1) - 2) * 2;
+    console.log(roznicaDlugosc + "dlugosci");
+
+    var dodajDlugosc = roznicaDlugosc * 150 + 950;
+
+    console.log("cena po dlugosci" + dodajDlugosc);
+    var roznica = (parseFloat(select2) - 3) * 2;
+
+    var dodajSzer = roznica * 250 + dodajDlugosc;
+
+
+    cena1.innerHTML = dodajSzer+"zl";
+
+}
 
 
 
-
-
-
+document.getElementById('show-price-now').addEventListener('click', sprawdz);
