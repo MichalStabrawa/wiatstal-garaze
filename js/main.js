60a7ee2f2d4e7e0bbf1613f8126ed61187daa05f
Michal Stabrawa <mistaaqua@gmail.com>
2018-05-12 21:59:33 +0200

    calculator js

--- js/main.js
+++ js/main.js
@@ -17,27 +17,45 @@ function sprawdz() {
 
 
     var select1 = document.getElementById('szerokosc').value;
-    
-    console.log(select1 +"szerokosc");
+
+    console.log(select1 + "szerokosc");
     var select2 = document.getElementById('dlugosc').value;
 
     var roznicaSzerokosc = (parseFloat(select1) - 2) * 2;
     console.log(roznicaSzerokosc + "szerokosc roznica");
 
     var dodajSzerokosc = roznicaSzerokosc * 250 + 950;
-    
-    console.log("Dodaj szerokosc" + dodajSzerokosc)
 
-   // console.log("cena po dlugosci" + dodajDlugosc);
     var roznica = (parseFloat(select2) - 3) * 2;
 
     var dodajSzer = roznica * 150 + dodajSzerokosc;
+    
+    
+    //transport value
+    var transportSelect=parseFloat(document.getElementById('transport').value);
+    
+    console.log(transportSelect);
+    
 
 
-    cena1.innerHTML ="CENA" +" "+ dodajSzer+"zl";
+    cena1.innerHTML = "CENA" + " " + (dodajSzer+ transportSelect)  +"zł";
+    
+    document.getElementById("price-szer").innerHTML="szerokość" +" "+ select1+"m";
+    
+    document.getElementById("price-wys").innerHTML= "wysokość" +" "+ select2+"m";
+    
+if(transportSelect=="0"){
+    document.getElementById('price-transport').innerHTML="TRANSPORT:malopolskie";
+}else if(transportSelect=="50"){
+     document.getElementById('price-transport').innerHTML= "TRANSPORT:śląskie"
+}
+
+
 
 }
 
 
 
 document.getElementById('show-price-now').addEventListener('click', sprawdz);
+
+
