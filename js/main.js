c2d799ee863601c7c3be9741706c2071803d9d37
Michal Stabrawa <mistaaqua@gmail.com>
2018-05-16 08:39:44 +0200

    calc cennik select option add

--- js/main.js
+++ js/main.js
@@ -6,23 +6,41 @@ $(document).ready(function () {
     $("#test").click(function () {
         $("#figure-spad-tyl").show();
         $("#calc1").show();
-            $('html, body').animate({
-        scrollTop: $('#figure-spad-tyl').offset().top
-    }, 1000);
-        
-   })
-    
-    $("#btn-header").click(function(){
-               $('html, body').animate({
-        scrollTop: $('#offert1').offset().top
-    }, 1000);
-    })
-    
-    
-    
-    /*scroll top button to section noclegi*/
+        $('html, body').animate({
+            scrollTop: $('#figure-spad-tyl').offset().top
+        }, 1000);
 
-    
+    })
+
+    $("#btn-header").click(function () {
+        $('html, body').animate({
+            scrollTop: $('#offert1').offset().top
+        }, 1000);
+    })
+
+
+
+    $(".target").change(function () {
+
+        var wartosc = parseFloat($(this).val());
+
+        const pobierzTextTable = $(this).parent().prev().attr('value');
+
+        const parsujText = parseFloat(pobierzTextTable);
+
+        $(this).parent().prev().text(wartosc + parsujText);
+
+
+
+
+
+    });
+
+})
+
+
+
+
 
 
 
@@ -40,50 +58,50 @@ function sprawdz() {
     var select2 = document.getElementById('dlugosc').value;
 
     var roznicaSzerokosc = (parseFloat(select1) - 2) * 2;
-   
+
 
     var dodajSzerokosc = roznicaSzerokosc * 75 + 950;
 
     var roznica = (parseFloat(select2) - 3) * 2;
 
     var dodajSzer = roznica * 75 + dodajSzerokosc;
-    
-    
+
+
     //transport value
-    var transportSelect=parseFloat(document.getElementById('transport').value);
-    
-    var garageHeight=parseFloat(document.getElementById("podwyzszenie").value);
-    
-    if(select1>"2" & select1<"3" &garageHeight!="0"){
-        garageHeight+= 10;
+    var transportSelect = parseFloat(document.getElementById('transport').value);
+
+    var garageHeight = parseFloat(document.getElementById("podwyzszenie").value);
+
+    if (select1 > "2" & select1 < "3" & garageHeight != "0") {
+        garageHeight += 10;
     }
-    
+
     //door variables
-    
-    var doorValue=parseFloat(document.getElementById('door').value);
+
+    var doorValue = parseFloat(document.getElementById('door').value);
     //window variables
-    windowValue=parseFloat(document.getElementById('window-price').value);
-    
-  
-    
+    windowValue = parseFloat(document.getElementById('window-price').value);
 
 
-    cena1.innerHTML = "CENA" + " " + (dodajSzer+ transportSelect + garageHeight + doorValue + windowValue)  +"zł";
-    
-    document.getElementById("price-szer").innerHTML="szerokość" +" "+ select1+"m";
-    
-    document.getElementById("price-wys").innerHTML= "wysokość" +" "+ select2+"m";
-    
-if(transportSelect=="0"){
-    document.getElementById('price-transport').innerHTML="TRANSPORT:malopolskie";
-}else if(transportSelect=="50"){
-     document.getElementById('price-transport').innerHTML= "TRANSPORT:śląskie"
-}
-    
-   var divCena= document.getElementById('cenaDiv1');
-    divCena.style.display="block";
-    
-var element=document.getElementById('cenaDiv1');
+
+
+
+    cena1.innerHTML = "CENA" + " " + (dodajSzer + transportSelect + garageHeight + doorValue + windowValue) + "zł";
+
+    document.getElementById("price-szer").innerHTML = "szerokość" + " " + select1 + "m";
+
+    document.getElementById("price-wys").innerHTML = "wysokość" + " " + select2 + "m";
+
+    if (transportSelect == "0") {
+        document.getElementById('price-transport').innerHTML = "TRANSPORT:malopolskie";
+    } else if (transportSelect == "50") {
+        document.getElementById('price-transport').innerHTML = "TRANSPORT:śląskie"
+    }
+
+    var divCena = document.getElementById('cenaDiv1');
+    divCena.style.display = "block";
+
+    var element = document.getElementById('cenaDiv1');
     element.scrollIntoView();
 
 }
@@ -91,5 +109,3 @@ var element=document.getElementById('cenaDiv1');
 
 
 document.getElementById('show-price-now').addEventListener('click', sprawdz);
-
-
